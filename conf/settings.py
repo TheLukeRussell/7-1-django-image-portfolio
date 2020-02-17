f37c0e707ef4ee54bbc21687284d7e2ce3b94dfd
Luke Russell <thelukerussel21@gmail.com>
2020-02-17 13:30:33 -0500

    added more images

--- conf/settings.py
+++ conf/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'images.apps.ImagesConfig',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- images/models.py
+++ images/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 class Image(models.Model):
-    title = models.CharField(max_length = 255)
-    author = models.CharField(max_length = 30)
-    description = models.TextField()
-    url = models.URLField()
+    title = models.CharField(max_length=30)
+    cover = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
